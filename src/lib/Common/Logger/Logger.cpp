8ec45d4f72d093cb3ef58e48662f6dccf3b863d4
Zachary Pike <zachturnpike@gmail.com>
2025-02-02 17:59:20 -0500

    Allow for compilation on linux

--- src/lib/Common/Logger/Logger.cpp
+++ src/lib/Common/Logger/Logger.cpp
@@ -1,6 +1,8 @@
 #include "Logger.hpp"
 
+#ifdef _WIN32
 #include <windows.h>
+#endif
 
 static std::mutex coutLock;
 
@@ -9,42 +11,66 @@ Logger::Logger(std::string name, Color c):
     color{c} {}
 
 void Logger::log(std::string msg) {
+    #ifdef _WIN32
     HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    #endif
 
     coutLock.lock();
 
+    #ifdef _WIN32
     SetConsoleTextAttribute(hConsole, static_cast<WORD>(color));
+    #endif
+
     printf("[%s] ", loggerName.c_str());
 
+    #ifdef _WIN32
     SetConsoleTextAttribute(hConsole, static_cast<WORD>(Color::WHITE));
+    #endif
+
     printf("[INFO] %s\n", msg.c_str());
 
     coutLock.unlock();
 }
 
 void Logger::warn(std::string msg) {
+    #ifdef _WIN32
     HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    #endif
 
     coutLock.lock();
 
+    #ifdef _WIN32
     SetConsoleTextAttribute(hConsole, static_cast<WORD>(color));
+    #endif
+
     printf("[%s] ", loggerName.c_str());
 
+    #ifdef _WIN32
     SetConsoleTextAttribute(hConsole, static_cast<WORD>(Color::YELLOW));
+    #endif
+
     printf("[WARN] %s\n", msg.c_str());
 
     coutLock.unlock();
 }
 
 void Logger::error(std::string msg) {
+    #ifdef _WIN32
     HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    #endif
 
     coutLock.lock();
 
+    #ifdef _WIN32
     SetConsoleTextAttribute(hConsole, static_cast<WORD>(color));
+    #endif
+
     printf("[%s] ", loggerName.c_str());
 
+    #ifdef _WIN32
     SetConsoleTextAttribute(hConsole, static_cast<WORD>(Color::RED));
+    #endif
+
     printf("[ERROR] %s\n", msg.c_str());
 
     coutLock.unlock();
